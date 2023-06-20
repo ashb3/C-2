a313bc123ead40548ee333a33a426abb20d0b0f7
barika <barika@SCE-0013>
2023-06-19 19:13:58 -0600

    Add project files.

---
+++ CSharpAssignment2/CSharpAssignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharpAssignment2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository<User> userRepository = new Repository<User>();
+            string addUser = userRepository.Add(new User());
+            Console.WriteLine(addUser);
+
+            string updateUser = userRepository.Update(new User());
+            Console.WriteLine(updateUser);
+
+            string deleteUser = userRepository.Delete(new User());
+            Console.WriteLine(deleteUser);
+
+            string getallUser = userRepository.GetAll();
+            Console.WriteLine(getallUser);
+
+         
+        }
+    }
+}
---
+++ CSharpAssignment2/CSharpAssignment2/Repository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment2
+{
+    public class Repository<T> where T : BaseEntity
+    {
+        public string Add(T entity)
+        {
+           
+            return $"Added {typeof(T).Name}";
+        }
+
+        public string Update(T entity)
+        {
+           
+            return $"Updated {typeof(T).Name}";
+        }
+
+        public string Delete(T entity)
+        {
+            
+            return $"Deleted {typeof(T).Name}";
+        }
+
+        public string GetAll()
+        {
+            
+            return $"Retrieved all {typeof(T).Name}";
+        }
+
+
+      
+    }
+
+
+}
